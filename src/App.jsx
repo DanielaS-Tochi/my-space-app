c546bc14d9895e875d8900efb4f136296c0f8478
DanielaS-Tochi <danielastochi@gmail.com>
2024-12-24 11:37:36 -0300

    Cabecera y CampoTexto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import styled from "styled-components"
+import GlobalStyles from "./components/GlobalStyles"
+import Cabecera from "./components/Cabecera"
+
+const FondoGradiente = styled.div`
+background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #144580 96.76%);
+width: 100%;
+min-height: 100vh;
+`
+
 function App() {
  
   return (
     <>
-      
+      <FondoGradiente>
+        <GlobalStyles />
+        <Cabecera />
+        </FondoGradiente>
+
     </>
   )
 }
